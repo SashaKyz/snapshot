b74d04ce212fb309026e2a2a796753d97909cdda
Tyler Clemens <Tyler.Clemens@osi.ca.gov>
2019-11-22 08:37:00 -0800

    Update tests

--- spec/javascripts/components/common/DateFieldStatefulSpec.jsx
+++ spec/javascripts/components/common/DateFieldStatefulSpec.jsx
@@ -82,12 +82,66 @@ describe('DateFieldStateful', () => {
   })
 
   describe('#handleOnBlur', () => {
-    it('converts to a YYYY-MM-DD date', () => {
+    it('sets an invalid date error and clears the value when the value is not a valid date string', () => {
+      const onBlur = jasmine.createSpy('onBlur')
+      const component = shallow(<DateFieldStateful value='asdf' onBlur={onBlur} />)
+      const instance = component.instance()
+      spyOn(instance, 'currentErrors').and.callThrough()
+      instance.handleOnBlur()
+      expect(onBlur).toHaveBeenCalled()
+      expect(instance.currentErrors).toHaveBeenCalled()
+      expect(instance.state.errors).toEqual(['Please enter a valid date'])
+      expect(component.state.value).toEqual(undefined)
+    })
+
+    it('clears errors if the shadowvalue is empty', () => {
+      const onBlur = jasmine.createSpy('onBlur')
+      const component = shallow(<DateFieldStateful value='' onBlur={onBlur} />)
+      const instance = component.instance()
+      spyOn(instance, 'currentErrors').and.callThrough()
+      instance.handleOnBlur()
+      expect(onBlur).toHaveBeenCalled()
+      expect(instance.currentErrors).toHaveBeenCalled()
+      expect(instance.state.errors).toEqual([])
+    })
+
+    it('clears errors if the shadowvalue is valid', () => {
+      const onBlur = jasmine.createSpy('onBlur')
+      const component = shallow(<DateFieldStateful value='10/10/2000' onBlur={onBlur} />)
+      const instance = component.instance()
+      spyOn(instance, 'currentErrors').and.callThrough()
+      instance.handleOnBlur()
+      expect(onBlur).toHaveBeenCalled()
+      expect(instance.currentErrors).toHaveBeenCalled()
+      expect(instance.state.errors).toEqual([])
+    })
+  })
+
+  describe('#handleOnChange', () => {
+    it('converts to a YYYY-MM-DD date and calls onChange', () => {
       const onChange = jasmine.createSpy('onChange')
       const component = mountDateFieldStateful({value: '', onChange})
       const instance = component.instance()
       instance.handleOnChange(new Date('01/01/2001'))
       expect(onChange.calls.mostRecent().args[0]).toEqual('2001-01-01')
+      expect(onChange).toHaveBeenCalled()
+    })
+
+    it('calls onchange if the date is null', () => {
+      const onChange = jasmine.createSpy('onChange')
+      const component = shallow(<DateFieldStateful onChange={onChange} />)
+      const instance = component.instance()
+      instance.handleOnChange(null)
+      expect(onChange).toHaveBeenCalled()
+    })
+
+    it('sets the value to the date', () => {
+      const date = new Date('10/10/2000')
+      const onChange = jasmine.createSpy('onChange')
+      const component = shallow(<DateFieldStateful onChange={onChange} />)
+      const instance = component.instance()
+      instance.handleOnChange(date)
+      expect(instance.state.value).toEqual(date)
     })
   })
 })
